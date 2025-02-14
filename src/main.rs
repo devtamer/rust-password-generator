e13f485fe901f7005b49382cac9a752d48ef4657
Tamer Elsawaf <tamer@muonspace.com>
2025-02-14 09:50:16 -0800

    working password generator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::env;
+use rand::{distr::Alphanumeric, Rng};
+
+fn password_generator(length: Option<u16>){
+    let mut password: String = Default::default();
+    let length = length.unwrap_or(12);
+    // longer method
+    for _ in 0..=length {
+        let randchar: char = rand::rng().sample(Alphanumeric)  as char;
+        password.push(randchar);
+    }
+    println!("Your new password is {password}");
+    // one liner method
+    //let rand_string: String = rand::rng().sample_iter(&Alphanumeric).take(length as usize).map(char::from).collect();
+    //println!("Your new password is {rand_string}");
+}
 fn main() {
-    println!("Hello, world!");
+   let args: Vec<String> = env::args().collect();
+   match args.len() {
+    1 => password_generator(None),
+    2 => {
+        let num: u16 = args[1]
+        .parse::<u16>()
+        .expect("Please enter a valid number between 1 and 65535");
+        password_generator(Some(num));
+    },
+    _ => ()
+   }
 }
